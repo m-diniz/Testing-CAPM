f61078f337b665c214ca8149cd09336ba1c2d9b4
Marcos Diniz <93732747+m-diniz@users.noreply.github.com>
2022-06-27 14:37:54 -0300

    Update Testing CAPM.R

--- Testing CAPM.R
+++ Testing CAPM.R
@@ -1,3 +1,4 @@
+# Capital Asset Pricing Model
 # Testing the CAPM for portfolios
 # Marcos Diniz
 # June-2022
@@ -7,16 +8,16 @@
 # Simulating data ---------------------------------------------------------
 set.seed(59)
 
-# 100 stocks
+# 100 portfolios
 i <- 100
 
 # 60 months
 t <- 60
 
-# Average returns of the firms
+# Simulate average returns of the firms
 mu_ret <- rnorm(i, 0, 0.01)
 
-# Standart deviations of returns
+# Simulate standart deviations of returns
 sd_ret <- runif(i, 0.03, 0.06)
 
 # Generating 60 monthly returns based on parameters mu_ret and sd_ret
@@ -27,23 +28,24 @@ for (j in 1:i){
 }
 
 # Simulating 60 monthly returns for risk free asset
-# Assuming the risk free rate will be constant 
+# Set risk free rate constant, in this example
 risk_free <- rep(0.001, t)
 my_data_excess <- apply(my_data, 2, function(x){x-risk_free})
 
 # Get the lowest the return with the lowest variance
 sd_rets <- apply(my_data, 2, sd)
-index <- match(sd_rets[which.min(sd_rets)], sd_rets) # column 54 will be the market portfolio
+index <- match(sd_rets[which.min(sd_rets)], sd_rets)
+# column 54 will be the market portfolio, because is expected low variance from market portfolio
 
 # Step (1): Run individual regressions -----------------------------------
 # We'll regress individual returns agains portfolio returns to get betas
-# Note1: regression of returns in EXCESS
+# Note 1: regression of returns in EXCESS
 betas <- apply(my_data_excess, 2, function(x){lm(x ~ my_data_excess[,index])$coefficients[2]})
 
 
 
 # Step (2): Run betas against average returns -----------------------------
-# Note2: regression of ACTUAL returns, not EXCESS returns
+# Note 2: regression of ACTUAL returns, not EXCESS returns
 # R_i_mean = λ0 + λ1*beta_i + v_i
 mean_ret <- colMeans(my_data)
 
